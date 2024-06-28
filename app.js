f33334251ab95d22fbbc7eef61c0b03e150887bd
Minhaz Jisun <minhaz.jisun@gmail.com>
2024-06-28 23:48:34 +0600

    Updated

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  // res.send('Hello World! Express server is running on port 5000.');
-  res.send('We did it!');
+  res.send('Hello World! Express server is running on port 5000.');
+  // res.send('We did it!');
 });
 
 app.listen(port, () => {
